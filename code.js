429fc97fb7526ce055dcd7c7fa5fce4a6d651fe0
JustRelaXD <178076675+JustRelaXD@users.noreply.github.com>
2025-01-31 18:01:13 +0530

    created playRound Function

--- code.js
+++ code.js
@@ -12,12 +12,43 @@ switch(randomNumber){
 }
 
 function getHumanChoice(){
-  let humanChoiceumanChoice=prompt("Enter your choice!")
-  return humanChoice;
+  let HumanChoice=prompt("Enter your choice!")
+  return HumanChoice.toLowerCase();
 }
 
 let HumanScore=0;
 let ComputerScore=0;
 
-HumanChoice=getHumanChoice();
-ComputerChoice=getComputerChoice();
+const humanChoice=getHumanChoice();
+const computerChoice=getComputerChoice();
+
+function playRound(humanChoice,computerChoice){
+  if(humanChoice==computerChoice){
+    winner='none';
+  }
+  
+  else if(humanChoice=='rock' && computerChoice=='scissor'){
+    winner='human';
+  }
+  
+  else if(humanChoice=='paper' && computerChoice=='rock'){
+    winner='human';
+  }
+  
+  else if(humanChoice=='scissor' && computerChoice=='paper'){
+    winner='human';
+  }
+  
+  else if(humanChoice=='rock' && computerChoice=='paper'){
+    winner='computer';
+  }
+  
+  else if(humanChoice=='paper' && computerChoice=='scissor'){
+    winner='computer';
+  }
+  
+  else if(humanChoice=='scissor' && computerChoice=='rock'){
+    winner='computer';
+  }
+}
+
